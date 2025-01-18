2ca0ab77c6edfe52735939031e5c0e6bdc0a95f9
Thisal Kenula <thisalkenula@outlook.com>
2025-01-18 10:27:06 +0530

    Refactor diffs.py to calculate consecutive differences until a constant difference is found

--- sequences/diffs.py
+++ sequences/diffs.py
@@ -1,10 +1,13 @@
 '''
 Find the difference between each consecutive pair of numbers in a sequence.
 '''
-seq = [2, 7, 15, 26, 40, 57]
+seq = [1, 2, 4, 9, 19, 36, 62, 99, 149]
 diffs = []
-
-for i in range(1, len(seq)):
-    diffs.append(seq[i] - seq[i - 1])
-
-print(diffs)
+while len(diffs) != 1:
+    diffs = []
+    for i in range(1, len(seq)):
+        diffs.append(seq[i] - seq[i - 1])
+    seq = diffs
+    print(diffs)
+    if all(diffs[i] == diffs[i - 1] for i in range(1, len(diffs))):
+        break
