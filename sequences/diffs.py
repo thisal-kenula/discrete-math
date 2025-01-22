1a10873fe4016c03daff090217924b77640086ba
Thisal Kenula <thisalkenula@outlook.com>
2025-01-22 10:41:58 +0530

    Update diffs.py: Avoid printing delta k for sequences with lengrh of 1

--- sequences/diffs.py
+++ sequences/diffs.py
@@ -1,7 +1,7 @@
 '''
 Find the difference between each consecutive pair of numbers in a sequence.
 '''
-seq = [1, 2, 4, 9, 19, 36, 62, 99, 149]
+seq = [1,2,4,8,16,32,64]
 diffs = []
 k = 0
 while len(diffs) != 1:
@@ -12,5 +12,5 @@ while len(diffs) != 1:
     seq = diffs
     print(diffs)
     if all(diffs[i] == diffs[i - 1] for i in range(1, len(diffs))):
-        print(f'Δ{k}-constant sequence')
+        if len(diffs) != 1: print(f'Δ{k}-constant sequence')
         break
