11280ee7c8198243a5a8b040620e5f7df084fdb4
Thisal Kenula <thisalkenula@outlook.com>
2025-01-22 10:39:37 +0530

    Enhance diffs.py to track the number of iterations until a constant difference is found

--- sequences/diffs.py
+++ sequences/diffs.py
@@ -3,11 +3,14 @@ Find the difference between each consecutive pair of numbers in a sequence.
 '''
 seq = [1, 2, 4, 9, 19, 36, 62, 99, 149]
 diffs = []
+k = 0
 while len(diffs) != 1:
+    k += 1
     diffs = []
     for i in range(1, len(seq)):
         diffs.append(seq[i] - seq[i - 1])
     seq = diffs
     print(diffs)
     if all(diffs[i] == diffs[i - 1] for i in range(1, len(diffs))):
+        print(f'Δ{k}-constant sequence')
         break
